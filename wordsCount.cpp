de63774b4a463be3a66256550a82f67bf9c2684d
Shivesh-Dixit <22103159@mail.jiit.ac.in>
2024-08-01 16:29:49 +0530

    second question

---
+++ wordsCount.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <string>
+using namespace std;
+int main() {
+string paragraph;
+cout << "Enter a paragraph: ";
+getline(cin, paragraph);
+stringstream ss(paragraph);
+string word;
+map<string, int> wordCount;
+while (ss >> word) {
+++wordCount[word];
+}
+cout << "\nWord Frequencies:\n";
+for (const auto& pair : wordCount) {
+cout << pair.first << ": " << pair.second << endl;
+}
+return 0;
+}
